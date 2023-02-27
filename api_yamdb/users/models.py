4dc69d2810d71c4bce69f87b7a6613cd567e8123
Артём Астапов <artem-astap@yandex.ru>
2023-02-27 13:45:00 +0300

    finish

---
+++ api_yamdb/users/models.py
@@ -0,0 +1,40 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    USER = 'user'
+    MODERATOR = 'moderator'
+    ADMIN = 'admin'
+    ROLE = [
+        ('user', USER),
+        ('moderator', MODERATOR),
+        ('admin', ADMIN),
+    ]
+    role = models.CharField(
+        max_length=40, choices=ROLE,
+        default=USER, verbose_name='Роль',
+    )
+    email = models.EmailField(max_length=60, blank=False, unique=True)
+    bio = models.TextField(max_length=250, blank=True)
+
+    class Meta:
+        ordering = ('-pk',)
+
+    @property
+    def is_admin(self):
+        return self.role == User.ADMIN
+
+    @property
+    def is_moderator(self):
+        return self.role == User.MODERATOR
+
+    def save(self, *args, **kwargs):
+        if self.role == self.MODERATOR:
+            self.is_staff = True
+        if self.role == self.ADMIN:
+            self.is_superuser = True
+        super(User, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.username
